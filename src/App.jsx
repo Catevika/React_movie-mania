ce2ad88d446d148f73f96c1e31ba3217c6174a9f
Catevika <dominique.bello@outlook.fr>
2023-01-12 13:11:39 +0100

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import RouteLayout from './components/RouteLayout/RouteLayout';
 import Home from './pages/Home/Home';
-import MovieDetails from './components/MovieDetails/MovieDetails';
+import MovieDetails from './pages/MovieDetails/MovieDetails';
 import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
 import Credits from './pages/Credits/Credits';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import NoMatch from './pages/noMatch/noMatch';
 import './App.css';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RouteLayout />} >
-      <Route index element={<Home />} />
-      <Route path='movies/:id' element={<MovieDetails />} />
-      <Route path='about' element={<About />} />
-      <Route path='credits' element={<Credits />} />
+    <Route path='/' element={<RouteLayout />} errorElement={<ErrorBoundary />} >
+      <Route index element={<Home />} errorElement={<ErrorBoundary />} />
+      <Route path='movie/:movieId' element={<MovieDetails />} errorElement={<ErrorBoundary />} />
+      <Route path='about' element={<About />} errorElement={<ErrorBoundary />} />
+      <Route path='contact' element={<Contact />} errorElement={<ErrorBoundary />} />
+      <Route path='credits' element={<Credits />} errorElement={<ErrorBoundary />} />
       <Route path='*' element={<NoMatch />} />
     </Route>
   )
