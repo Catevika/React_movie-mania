b688a284a813b5b93962a06c89777ae85b5f045f
Catevika <dominique.bello@outlook.fr>
2023-01-12 13:12:48 +0100

    Add Credits

--- src/pages/Credits/Credits.jsx
+++ src/pages/Credits/Credits.jsx
@@ -4,11 +4,13 @@ import './Credits.css';
 export default function Credits() {
   return (
     <div className='credits-container'>
-      <h2>Credits</h2>
-      <div className='logo-container'>
-        <img title='The Movie Database' src={TMDBlogo} alt="TMDB logo" />
+      <div className='credits-wrapper'>
+        <h2>Credits</h2>
+        <div className='logo-container'>
+          <img title='The Movie Database' src={TMDBlogo} alt="TMDB logo" />
+        </div>
+        <p>This product uses The Movie Database (TMDB) API but is not endorsed or certified by TMDB.</p>
       </div>
-      <p>This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
     </div>
   );
 }
