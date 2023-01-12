3ee414d2f61905b5b4f626adf1b407dacf4ac730
Catevika <dominique.bello@outlook.fr>
2023-01-12 13:13:13 +0100

    Update RouteLayout

--- src/components/RouteLayout/RouteLayout.jsx
+++ src/components/RouteLayout/RouteLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import './RouteLayout.css';
 
 export default function RouteLayout() {
   return (
