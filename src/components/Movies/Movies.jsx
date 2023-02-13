a50fe2622b4cef3c9ad4e3a33ba99a346f75caa3
Catevika <dominique.bello@outlook.fr>
2023-02-13 16:11:29 +0100

    Refactor: extract fetching - components to api

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -5,26 +5,19 @@ import './Movies.css';
 
 
 export default function Movies({ term }) {
-  const [error, setError] = useState(false);
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    try {
-      const getMovies = async () => {
-        setError(false);
-        const response = await getMovieData(term);
-        setMovies(response.data.results);
-      };
-      if (term) getMovies();
-    } catch (error) {
-      setError(true);
-      console.log({ errror: error.message });
-    }
+    const getMovies = async () => {
+      const response = await getMovieData(term);
+      response && setMovies(response.data.results);
+    };
+    if (term) getMovies();
   }, [term]);
 
   return (
     <div className='movies-container'>
-      {!error && movies?.length > 0 ? movies.map(movie => {
+      {movies?.length > 0 ? movies.map(movie => {
         return <div className='movie-container' key={movie.id}><Movie movie={movie} /></div>;
       }) : <div className='message'>No movie available yet.</div>}
     </div>
