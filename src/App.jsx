81aaf514548577587d4364644dc9b2a888e5d5da
Catevika <dominique.bello@outlook.fr>
2023-02-13 16:23:43 +0100

    Update NoMatch  Vercel + Router + btn outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import RouteLayout from './components/RouteLayout/RouteLayout';
 import Home from './pages/Home/Home';
 import MovieDetails from './pages/MovieDetails/MovieDetails';
@@ -6,27 +6,23 @@ import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
 import Credits from './pages/Credits/Credits';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
-import NoMatch from './pages/NoMatch/NoMatch';
 import './App.css';
-
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<RouteLayout />} errorElement={<ErrorBoundary />} >
-      <Route index element={<Home />} errorElement={<ErrorBoundary />} />
-      <Route path='movie/:movieId' element={<MovieDetails />} errorElement={<ErrorBoundary />} />
-      <Route path='about' element={<About />} errorElement={<ErrorBoundary />} />
-      <Route path='contact' element={<Contact />} errorElement={<ErrorBoundary />} />
-      <Route path='credits' element={<Credits />} errorElement={<ErrorBoundary />} />
-      <Route path='*' element={<NoMatch />} />
-    </Route>
-  )
-);
 
 function App() {
 
   return (
-    <RouterProvider router={router} />
+    <>
+      <Routes>
+        <Route path='/' element={<RouteLayout />} errorElement={<ErrorBoundary />} end>
+          <Route index element={<Home />} errorElement={<ErrorBoundary />} />
+          <Route path='movie/:movieId' element={<MovieDetails />} errorElement={<ErrorBoundary />} />
+          <Route path='about' element={<About />} errorElement={<ErrorBoundary />} />
+          <Route path='contact' element={<Contact />} errorElement={<ErrorBoundary />} />
+          <Route path='credits' element={<Credits />} errorElement={<ErrorBoundary />} />
+        </Route>
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
